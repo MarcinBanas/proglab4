ebc1c07190a220b369df2bbdb914a11079e8a475
Marcin Banaś <mb056625@student.ath.edu.pl>
2020-11-10 10:44:46 +0100

    Dodaj pliki projektów.

---
+++ klasycs/Ksiazka.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace klasycs
+{
+    public class Ksiazka
+    {
+        public string Tytul { get; set; }
+        public string Autor { get; set; }
+        public int Regal { get; set; }
+        public int Polka { get; set; }
+        public int Miejsce { get; set; }
+
+        public Ksiazka(string tytul,string autor,int regal,int polka,int miejsce)
+        {
+            Tytul = tytul;
+            Autor = autor;
+            Regal = regal;
+            Polka = polka;
+            Miejsce = miejsce;
+        }
+       
+    }
+}
---
+++ klasycs/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace klasycs
+{
+    class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+            Ksiazka[,,] Biblioteka = new Ksiazka[3, 6, 10];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    for (int k = 0; k < 10; k++)
+                    {
+                        Biblioteka[i, j, k] = new Ksiazka("Hobbit", "Tolkien", i, j, k);
+                    }
+                }
+            }
+            int a=0;
+            Biblioteka[0, 0, 9].Autor = "Banas";
+            Biblioteka[0, 0, 9].Tytul = "Marcin";
+            Console.WriteLine("Podaj tytul lub autora ksiazki:");
+            string x = Console.ReadLine();
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    for (int k = 0; k < 10; k++)
+                    {
+                        if (Biblioteka[i, j, k].Autor.Contains(x) == true || Biblioteka[i, j, k].Tytul.Contains(x) == true)
+                        {
+                            Console.WriteLine("Tytul: " + Biblioteka[i, j, k].Tytul + " Autor: " + Biblioteka[i, j, k].Autor + " Nr regalu: " + Biblioteka[i, j, k].Regal + " Nr polki: " + Biblioteka[i, j, k].Polka + " Nr miejsca: " + Biblioteka[i, j, k].Miejsce);
+                            a++;                        
+                        }
+                    }
+                }
+            }
+            if (a == 0)
+                Console.WriteLine("Podany tytul/autor nie pasuje do zadnej ksiazki.");
+        }
+    }
+}
